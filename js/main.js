ddd0b9f5249686dc42903c77ccd296804061a947
Roel van der Ven <hello@roelvanderven.com>
2014-08-21 12:01:44 +0200

    v1

---
+++ js/main.js
@@ -0,0 +1,39 @@
+(function(){
+  ZeroClipboard.config(
+    {
+      swfPath: 'js/vendor/ZeroClipboard.swf'
+    }
+  );
+
+  var client = new ZeroClipboard(document.getElementById('btn_copy'));
+
+  client.on('ready', function(readyEvent) {
+
+    client.on('aftercopy', function(event) {
+      event.target.style.display = "none";
+      console.log("Copied text to clipboard: " + event.data["text/plain"]);
+    });
+
+  });
+
+  $('input#url').blur(function() {
+    var link = $(this).val();
+
+    if (link != '') {
+      $.ajax({
+        url: 'http://www.tape.tv/services/oembed?url=' + link,
+        success: function(data) {
+          var complete_embedcode = data.html + '<p><a href="' + link + '?utm_source=tape.tv&utm_campaign=koop&utm_medium=embed">' + data.title + '</a> auf <a href="http://www.tape.tv/?utm_source=tape.tv&utm_campaign=koop&utm_medium=embed">tape.tv</a></p>';
+          $('#embedcode').html(complete_embedcode);
+          $('#btn_copy').attr('data-clipboard-text', complete_embedcode);
+          $('#embedcode').focus().select();
+        },
+        error: function (data) {
+          $('span.error').html('Eek! That does not look like a tape.tv link. Computer says <i>"' + data.statusText + '"</i>.');
+          $('#btn_copy').attr('data-clipboard-text', '');
+        }
+      });
+    };
+  });
+
+})();
